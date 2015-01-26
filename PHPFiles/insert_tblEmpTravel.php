1d97a1c84a24e119a3bb9e6c7b82799322ffdaf2
jeffman14 <mjc.tops@gmail.com>
2015-01-27 03:00:20 +0800

    Update insert_tblEmpTravel.php
    NOTE: the filename in hosting has been change to insertUpdate_tblEmpTravel.php
    Solved duplicates when saving. But has bug on response, I couldn't retrieve the json response, it doesn't proceed to next activity.

--- PHPFiles/insert_tblEmpTravel.php
+++ PHPFiles/insert_tblEmpTravel.php
@@ -1,5 +1,8 @@
 <?php
-
+//0 error
+//1 already signed in
+//2 travelno not found
+//3 inserted
 
 // check for required fields
 if (isset($_POST['driverid'])&& isset($_POST['aideid']) && isset($_POST['travelno'])) {
@@ -15,9 +18,31 @@ if (isset($_POST['driverid'])&& isset($_POST['aideid']) && isset($_POST['traveln
     // connecting to db
     $db = new DB_CONNECT();
 
-    // mysql inserting a new row
-    $result = mysql_query("INSERT INTO tblemptravel(touraideno, driverno, travelno) VALUES('$aideid', '$driverid', '$travelno')");
-// echoing JSON response
-    echo json_encode($result);
+    $query = "SELECT * FROM tbltravel where travelno='$travelno'"; 
+    $q=mysql_query($query);
+    $rowCount = mysql_num_rows($q);
+    
+    if ($rowCount != 0 ){
+        //travelno found in tbltravel
+        //search for travelno in tblEmpTravel
+        $query = "SELECT * FROM tblemptravel where travelno='$travelno'"; 
+        $q=mysql_query($query);
+        $rowCount = mysql_num_rows($q);
+        
+        //if found
+        if ($rowCount != 0 ){
+        //travelno found in tblemptravel
+        }
+        
+        else{
+        // insert travelno in tblEmpTravel
+        $result = mysql_query("INSERT INTO tblemptravel(touraideno, driverno, travelno) VALUES('$aideid', '$driverid', '$travelno')");
+        // echoing JSON response
+        $response["success"] = 1;
+        echo json_encode("Trial ni");
+     } 
+   }
+echo json_encode("aksa");
+ 
 }
-?>	
+?>		
